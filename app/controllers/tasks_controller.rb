e27b4f8e51284137e523617280615fc89536b9ef
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-10-15 13:28:14 -0400

    Clean up views and flash messages, add standard

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,113 +1,112 @@
 class TasksController < ApplicationController
-require "csv"
+  require "csv"
 
-before_action :set_task, except: [:index, :new, :create, :export_csv]
+  before_action :set_task, except: [:index, :new, :create, :export_csv]
 
-def index
-  fetch_possibly_filtered_tasks
-end
-
-def show
-
-end
-
-def new
-  @task = Task.new
-end
-
-
-def create
-  @task = Task.create(task_params)
-  respond_to do |format|
-    if @task.persisted?
-      format.html { redirect_to @task, notice: "Task successfully created!" }
-      format.json { render json: @task.as_json(only: [:title, :due_date, :status]), status: :created }
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity }
-    end
+  def index
+    fetch_possibly_filtered_tasks
   end
-end
 
-def edit
+  def show
 
-end
-
-def update
-  if @task.update(task_params)
-    respond_to do |format|
-      format.html { redirect_to @task, notice: "Task successfully updated!" }
-      format.json { render json: @task.as_json(only: [:title, :due_date, :status]), status: :ok }
-    end
-  else
-    respond_to do |format|
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity }
-    end
   end
-end
+
+  def new
+    @task = Task.new
+  end
 
 
-def destroy
-  @task.destroy
-  redirect_to tasks_url, notice: "Task deleted"
-end
-
-def mark_task_as_done
-  @task.update(status: true)
-  redirect_to @task
-end
-
-def export_csv
-  fetch_possibly_filtered_tasks
-
-  if @tasks.present?
+  def create
+    @task = Task.create(task_params)
     respond_to do |format|
-      format.csv do
-        send_data generate_csv(@tasks), filename: "tasks.csv"
+      if @task.persisted?
+        format.html { redirect_to @task, notice: "Task successfully created!" }
+        format.json { render json: @task.as_json(only: [:title, :due_date, :status]), status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity }
       end
     end
-  else
-    redirect_to tasks_url, notice: "No tasks to export"
   end
-end
 
-private
+  def edit
 
-def generate_csv(tasks)
-  CSV.generate(headers: true) do |csv|
-    csv << ["Title", "Date Created", "Status"]
-
-    @tasks.each do |task|
-      csv << [
-        task.title,
-        formatted_date(task.created_at),
-        task.status ? 'completed' : 'incomplete',
-      ]
-    end
   end
-end
 
-  def fetch_possibly_filtered_tasks
-    if params[:status] == 'completed'
-      @tasks = Task.completed
-    elsif params[:status] == 'incomplete'
-      @tasks = Task.incomplete
+  def update
+    if @task.update(task_params)
+      respond_to do |format|
+        format.html { redirect_to @task, notice: "Task successfully updated!" }
+        format.json { render json: @task.as_json(only: [:title, :due_date, :status]), status: :ok }
+      end
     else
-      @tasks = Task.all # With a lot of tasks in a production app, we would use pagination here
+      respond_to do |format|
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity }
+      end
     end
   end
 
-  def formatted_date(time)
-    time.strftime("%Y-%m-%d")
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: "Task deleted"
   end
 
-  def set_task
-    @task = Task.find(params[:id])
+  def mark_task_as_done
+    @task.update(status: true)
+    redirect_to @task, notice: "Get it, girl!"
   end
 
-  def task_params
-    params.require(:task).permit(:title, :due_date, :status, :description)
+  def export_csv
+    fetch_possibly_filtered_tasks
+
+    if @tasks.present?
+      respond_to do |format|
+        format.csv do
+          send_data generate_csv(@tasks), filename: "tasks.csv"
+        end
+      end
+    else
+      redirect_to tasks_url, notice: "No tasks to export"
+    end
   end
+
+  private
+
+  def generate_csv(tasks)
+    CSV.generate(headers: true) do |csv|
+      csv << ["Title", "Date Created", "Status"]
+
+      @tasks.each do |task|
+        csv << [
+          task.title,
+          formatted_date(task.created_at),
+          task.status ? 'completed' : 'incomplete',
+        ]
+      end
+    end
+  end
+
+    def fetch_possibly_filtered_tasks
+      if params[:status] == 'completed'
+        @tasks = Task.completed
+      elsif params[:status] == 'incomplete'
+        @tasks = Task.incomplete
+      else
+        @tasks = Task.all # With a lot of tasks in a production app, we would use pagination here
+      end
+    end
+
+    def formatted_date(time)
+      time.strftime("%Y-%m-%d")
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :due_date, :status, :description)
+    end
 
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -19,5 +19,4 @@ class Task < ApplicationRecord
   def has_description?
     description.present?
   end
-
 end
